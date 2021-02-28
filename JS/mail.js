9b1e7c20b3a4ace215f2e907b0d90a0bf8e802d1
itdesigntk <66639356+itdesigntk@users.noreply.github.com>
2021-02-27 22:48:48 -0600

    Removed the message

--- JS/mail.js
+++ JS/mail.js
@@ -90,9 +90,7 @@ function handleSignoutClick(event) {
 * @param {string} message Text to be placed in pre element.
 */
 function appendPre(message) {
- var pre = document.getElementById("content");
- var textContent = document.createTextNode(message + "\n");
- pre.appendChild(textContent);
+
 }
 
 /**
